6fff70c7d7760d41e0c863be0341c03cb1292611
Folami <folami.bidmus@gmail.com>
2026-01-21 19:59:43 +0100

    Carousel reponsiveness

--- src/FeaturedProducts.jsx
+++ src/FeaturedProducts.jsx
@@ -14,13 +14,14 @@ const FeaturedProducts = () => {
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 5000,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
+          arrows: false,
         },
       },
       {
